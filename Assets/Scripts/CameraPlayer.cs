157c36e4da9979342cb1323717e3009f102f9ba1
Adriluminati <adri99rey3@gmail.com>
2020-11-15 13:28:10 +0100

    first commit

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    private Camera cameraFollowPlayer;
+    
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFollowPlayer = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CameraFollow()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        CameraFollow();
+    }
+}
